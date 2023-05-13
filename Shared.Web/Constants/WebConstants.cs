4d0374f6d0c7dbb87cebe975e85e29700f29cc20
binhkhuu <binhkhuu90@gmail.com>
2023-05-13 15:47:14 +0800

    global nav and tests

--- Shared.Web/Constants/WebConstants.cs
+++ Shared.Web/Constants/WebConstants.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,11 @@ namespace Common.Web.Constants
     public static class WebConstants
     {
         public static readonly string LandingPageWelcome = "TODO: LandingPage Message";
+
+        public static class ComponentIDs
+        {
+            public static readonly string GlobalNav = "GlobalNav";
+            public static readonly string LandingPage = "LandingPage";
+        }
     }
 }
--- TypingSPA.Tests/Web/HomePage.Test.cs
+++ TypingSPA.Tests/Web/HomePage.Test.cs
@@ -1,10 +1,14 @@
 ﻿using Bunit;
 using Common.Web.Constants;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using MudBlazor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace TypingSPA.Tests.Web
 {
@@ -18,11 +22,37 @@ namespace TypingSPA.Tests.Web
         {
             using var ctx = new TestContext();
             var comp = ctx.RenderComponent<TypingSPA.Web.Pages.Index>();
-            var headingElement = comp.Find("#LandingPageWelcome");
-
+            var headingElement = comp.Find($"#{WebConstants.ComponentIDs.LandingPage}");
+            
             var typingMessage = headingElement.TextContent;
             Assert.Equal(typingMessage, WebConstants.LandingPageWelcome);
 
         }
+
+        [Fact]
+        public void ShouldLoadGlobalNav()
+        {
+            using var ctx = new TestContext();
+            var comp = ctx.RenderComponent<TypingSPA.Web.Shared.MainLayout>();
+            var headingElement = comp.Find($"#{WebConstants.ComponentIDs.GlobalNav}");
+
+            var exists = headingElement != null;
+            Assert.True(exists);
+        }
+
+        /// <summary>
+        /// sample inject test code
+        /// </summary>
+        public void teststuff()
+        {
+            var mockDialogService = new Mock<IDialogService>();
+            var mockSnackBarService = new Mock<ISnackbar>();
+            var mockMudPopoverProviderService = new Mock<IMudPopoverService>();
+            using var ctx = new TestContext();
+            ctx.Services.AddSingleton(mockDialogService.Object);
+            ctx.Services.AddSingleton(mockSnackBarService.Object);
+            ctx.Services.AddSingleton(mockMudPopoverProviderService.Object);
+        }
+
     }
 }
