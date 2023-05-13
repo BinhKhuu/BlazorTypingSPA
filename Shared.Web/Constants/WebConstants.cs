4fdaeb0085814fdf430477867f7fb041b2ce3463
binhkhuu <binhkhuu90@gmail.com>
2023-05-13 15:07:33 +0800

    Set up test proj.

---
+++ Shared.Web/Constants/WebConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Web.Constants
+{
+    public static class WebConstants
+    {
+        public static readonly string LandingPageWelcome = "TODO: LandingPage Message";
+    }
+}
---
+++ TypingSPA.Tests/Web/HomePage.Test.cs
@@ -0,0 +1,28 @@
+﻿using Bunit;
+using Common.Web.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypingSPA.Tests.Web
+{
+    public class HomePage
+    {
+
+        
+
+        [Fact]
+        public void ShouldLoadPage()
+        {
+            using var ctx = new TestContext();
+            var comp = ctx.RenderComponent<TypingSPA.Web.Pages.Index>();
+            var headingElement = comp.Find("#LandingPageWelcome");
+
+            var typingMessage = headingElement.TextContent;
+            Assert.Equal(typingMessage, WebConstants.LandingPageWelcome);
+
+        }
+    }
+}
